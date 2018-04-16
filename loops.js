4a4ce86bb6c28083bd49c23a2c63fc566472a230
putu eka mulyana <putuekamulyana97@gmail.com>
2018-04-16 08:58:25 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,13 @@
+   function forLoop(array){
+      for (let i = 0; i < array.length; i++) {
+        console.log(`I am ${i} strange loops.`);
+      }
+      }
+    
+const n = Math.floor(Math.random() * 100);
+function whileLoop(n){
+while(n > 0){
+  console.log(--n)
+}
+ console.log("done")
+}
