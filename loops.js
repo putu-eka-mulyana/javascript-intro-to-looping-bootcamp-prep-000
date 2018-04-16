6ec1cf413c82a895887e6a531d428259591b1aa9
putu eka mulyana <putuekamulyana97@gmail.com>
2018-04-16 09:00:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,13 +1,29 @@
-   function forLoop(array){
-      for (let i = 0; i < array.length; i++) {
-        console.log(`I am ${i} strange loops.`);
-      }
-      }
-    
-const n = Math.floor(Math.random() * 100);
-function whileLoop(n){
-while(n > 0){
-  console.log(--n)
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
+  }
+  return array;
 }
- console.log("done")
+
+function whileLoop(n) {
+  let countdown = n;
+    while (countdown > 0) {
+      console.log(--countdown)
+    }
+    return "done";
+  }
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
 }
